0f279d1bcdd0dc833bc08bcc3ebd5d3979028ad3
Cesahz <espinolac999@gmail.com>
2026-03-15 19:01:04 -0300

    establecer cascaron del motor logico y delegacion de funciones

---
+++ motor.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+#variables globales de memoria
+CLASES_DISPONIBLES = {
+    #ejemplo temporal para que entiendan como se crea un preset
+    "Heredero": {
+        "descripcion": "hijo de empresario. dinero de sobra, poca resistencia al estres.",
+        "stats": {"salud": 60, "social": 80, "intelecto": 40, "laboral": 30, "dinero": 5000000}
+    },
+    "Pobre pero Honrado": {
+        "descripcion": "acostumbrado a la presion. poco dinero, alta resistencia.",
+        "stats": {"salud": 90, "social": 40, "intelecto": 50, "laboral": 70, "dinero": 100000}
+    },
+    "Inteligente asocial": {
+        "descripcion": "inteligencia pura, cero habilidades sociales.",
+        "stats": {"salud": 50, "social": 10, "intelecto": 95, "laboral": 20, "dinero": 250000}
+    }
+}
+#en este van las stats
+estado_jugador = {}
+CARTAS = {}
+
+
+def cargar_cartas():
+    pass
+
+def inicializar_clase(clase):
+    pass
+
+def obtener_estado_actual(): # analizar en la plantilla_de_trabajo como funciona e implementar
+    pass
+
+def procesar_turno(eleccion):
+    pass
+
