331beaa8fc6b5469b95fae189efdb422d4cbfd14
JVGC <juan.vicente.gonzalez93@gmail.com>
2026-03-15 22:44:02 -0300

    feat: completar función obtener_estado_actual en motor.py

--- motor.py
+++ motor.py
@@ -29,7 +29,18 @@ def inicializar_clase(clase):
     pass
 
 def obtener_estado_actual(): # analizar en la plantilla_de_trabajo como funciona e implementar
-    pass
+    #.get(clave, valor_por_defecto): busca la clave en el dicc, si no existe devuelve el valor_por_defecto en vez de explotar
+    indice = estado_jugador.get("indice_carta", 0)
+    # verificar si todavia quedan cartas por jugar
+    if indice < len(CARTAS):
+        # se guarda la carta correspondiente al estado actual, si quedan cartas
+        carta_actual = CARTAS[indice]
+        # devolver estado del jugador, carta actual y bandera de juego "fin" = False, el juego continua
+        return ({"stats": estado_jugador, "carta": carta_actual, "fin": False})
+    else:
+        # en caso contrario no quedan cartas, devolver estado del jugador, carta actual y bandera de juego "fin" = True, el juego termina 
+        return ({"stats": estado_jugador, "carta": None, "fin": True, "mensaje": "Sobreviviste al MVP."})
+
 
 def procesar_turno(eleccion):
     pass
